f00f609f2cd90bc6e5a48467dfb0449453525658
Emilio P Egido <yo@iHACK13R.local>
2026-02-23 18:25:27 +0100

    Integrate Sparkle auto-updates
    - Add Sparkle via SwiftPM and wire up in the app
    - Update Info.plist (SUFeedURL and SUPublicEDKey)
    - Add appcast.xml
    - MARKETING_VERSION bumped to 1.5.0 and CURRENT_PROJECT_VERSION set to 31.

---
+++ Xattr-remove/UpdateController.swift
@@ -0,0 +1,24 @@
+//
+//  UpdaterController.swift
+//
+//  Created by perez987 on 2025-08-25
+//
+
+import Sparkle
+
+// View model that publishes when checks for updates can be performed by the user
+final class UpdaterController: ObservableObject {
+    private let updaterController: SPUStandardUpdaterController
+
+    @Published var canCheckForUpdates = false
+
+    init() {
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+        updaterController.updater.publisher(for: \.canCheckForUpdates)
+            .assign(to: &$canCheckForUpdates)
+    }
+
+    func checkForUpdates() {
+        updaterController.updater.checkForUpdates()
+    }
+}
--- Xattr-remove/Xattr_removeApp.swift
+++ Xattr-remove/Xattr_removeApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import Sparkle
 
 @main
 struct Xattr_rmApp: App {
     @StateObject private var fileProcessor = FileProcessor()
     @State private var isLanguageSelectorPresented = false
-    
+    @StateObject private var updaterController = UpdaterController()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -22,6 +24,20 @@ struct Xattr_rmApp: App {
         }
         .windowResizability(.contentSize)
         .commands {
+            CommandGroup(after: .appInfo) {
+                Button(
+                    NSLocalizedString(
+                        "Check for Updates...",
+                        comment: "Menu item to check for app updates"
+                    ),
+//                    systemImage: "square.and.arrow.down.badge.checkmark"
+                    systemImage: "arrow.triangle.2.circlepath"
+                ) {
+                    updaterController.checkForUpdates()
+                }
+                .keyboardShortcut("u", modifiers: [.command])
+                .disabled(!updaterController.canCheckForUpdates)
+            }
             CommandGroup(replacing: .newItem) { }
             CommandMenu(NSLocalizedString("menu_language", comment: "Language menu")) {
                 Button(NSLocalizedString("menu_select_language", comment: "Select Language menu item")) {
